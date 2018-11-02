6cd1a55f197c47a0bc5c419054ffdc50388b86f4
Kenneth Phang <bunnyppl@gmail.com>
2018-11-02 14:21:31 +0800

    new

--- aws-polly-example.js
+++ aws-polly-example.js
@@ -1,6 +1,6 @@
 // Example Node.js AWS Polly Script that saves an mp3 file to S3
 const AWS = require('aws-sdk')
-
+const fs = require('fs');
 AWS.config.loadFromPath('./awscreds.json');
 
 const Polly = new AWS.Polly({
@@ -9,31 +9,27 @@ const Polly = new AWS.Polly({
 })
 
 const s3 = new AWS.S3();
-
+//<amazon:effect name="shout"><prosody rate="fast"></prosody></amazon:effect>
 let pollyparams = {
-    'Text': '<speak>Hello, this is an example Node.js script which plays an audio stream converted by AWS Polly. <amazon:effect name="whispered"><prosody rate="slow">It is really cool.</prosody></amazon:effect></speak>',
+    'Text': '<speak>Hello, I am LuLu from Singapoor. How are you ? It is really cool !</speak>',
     'TextType': "ssml", 
     'OutputFormat': 'mp3',
-    'VoiceId': 'Amy'
+    'VoiceId': 'Zhiyu'
 }
 
 Polly.synthesizeSpeech(pollyparams, (err, data) => {
     if (err) {
         console.log(err.message)
     } else if (data) {
-        let s3params = {
-            Body: data.AudioStream, 
-            Bucket: "<Your Bucket>", 
-            Key: "<Your File Name>",
-            ACL: "public-read"
-        };
-
-        s3.upload(s3params, function(err, data) {
-            if (err) {
-                console.log(err.message);
-            } else {    
-                console.log(data.Location);
-            }
-        });
+        console.log(data);
+        
+        if (data.AudioStream instanceof Buffer) {
+            fs.writeFile('./test.mp3', data.AudioStream, function(err) 
+            { 
+              if (err) {
+                return console.log(err)
+              }
+            })
+          }
     }
 })
